43785971ae6fa89b15036b762103c98d4b98000d
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-06-11 21:54:09 -0300

    validação do formulario

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,18 +1,48 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, useCallback, useEffect, useRef, useState } from "react";
 import { IconBaseProps } from 'react-icons'
 
 import { Container } from './styles';
+import { useField } from "@unform/core";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
     icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-    <Container>
-        {Icon && <Icon />}
-        <input {...rest}></input>
-    </Container>
-)
+const Input: React.FC<InputProps> = ({ icon: Icon, name: nameField, ...rest }) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [focused, setisFocused] = useState(false);
+    const [filled, setIsFilled] = useState(false);
+
+    const { error, fieldName, defaultValue, registerField } = useField(nameField)
+
+    const handleInputFocus = useCallback(() => {
+        setisFocused(true);
+    }, []);
+
+    const handleInputBlur = useCallback(() => {
+        setisFocused(false);
+        setIsFilled(!!inputRef.current?.value);
+    }, []);
+
+    useEffect(() => {
+        registerField({ name: fieldName, ref: inputRef.current, path: 'value' })
+    }, [fieldName, registerField])
+
+    return (
+        <Container $isFilled={filled} $isFocused={focused}>
+            {Icon && <Icon />}
+            <input
+                ref={inputRef}
+                onFocus={handleInputFocus}
+                defaultValue={defaultValue}
+                onBlur={handleInputBlur}
+                {...rest}
+            ></input>
+            {error}
+        </Container>
+    )
+}
 
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { Background, Container, Content } from "./styles";
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import Input from "../../components/input";
 import Button from "../../components/button";
-
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web';
+import getValidationErrors from "../../utils/getValidationErrors";
 const SignUp: React.FC = () => {
+
+
+    const formRef = useRef<FormHandles>(null);
+
+    const handleSubmit = useCallback(async (event: React.FormEvent<Element> | undefined, data:object) => {
+        event?.preventDefault();
+
+        try {
+            formRef.current?.setErrors({})
+            const schema = Yup.object().shape({
+                name: Yup.string().required("Nome obrigatório"),
+                email: Yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+                password: Yup.string().min(6, 'No mínimo 6 caracteres'),
+            });
+            await schema.validate(data, { abortEarly: false });
+
+        } catch (err) {
+            if (err) {
+                const errors = getValidationErrors(err as Yup.ValidationError);
+                formRef.current?.setErrors(errors);
+            }
+        }
+    }, [])
     return (
         <Container>
             <Background />
             <Content>
                 <img src={logo} alt="GoBarber" />
-                <form>
-                    <h1>Faça seu Cadastro</h1>
-                    <Input icon={FiUser} placeholder="Nome" name="name" />
-                    <Input icon={FiMail} placeholder="e-mail" name="e-mail" />
-                    <Input icon={FiLock} placeholder="password" name="password" type="password" />
-                    <Button type="submit" >CADASTRAR</Button>
+                <Form ref={formRef} onSubmit={(data, helpers, event) => handleSubmit(event, data)} children=
+                    {<>
+                        <h1>Faça seu Cadastro</h1>
+                        <Input icon={FiUser} placeholder="Nome" name="name" />
+                        <Input icon={FiMail} placeholder="e-mail" name="email" />
+                        <Input icon={FiLock} placeholder="password" name="password" type="password" />
+                        <Button type="submit" >CADASTRAR</Button>
+                    </>
+                    }
+                    placeholder=''
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
+                />
 
-                </form>
                 <a href="/" >
                     <FiArrowLeft />
                     Voltar para LogOn</a>
             </Content>
-        </Container>
+        </Container >
     );
 }
 
